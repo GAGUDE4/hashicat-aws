78843b949a9baeac69ddc8e7c2e37abc33071100
gagude4 <gagude4@bancodebogota.com.co>
2022-10-06 23:06:31 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,5 +1,13 @@
 module "s3-bucket" {
   source  = "app.terraform.io/BancodeBogotaSanti/s3-bucket/aws"
   version = "2.8.0"
-}
 
+
+  bucket = "my-s3-bucket"
+  acl    = "private"
+
+  versioning = {
+    enabled = true
+  }
+
+}
